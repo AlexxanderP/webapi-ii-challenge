c88b07829e6898544b26b3a847a2eaa9c6d6ea01
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-03-19 15:34:08 -0700

    put and get posts by id created

--- data/postsRouter.js
+++ data/postsRouter.js
@@ -31,6 +31,18 @@ router.get("/", (req, res) => {
     });
 });
 
+//-------- Get Posts By Id--------//
+
+router.get("/:id", (req, res) => {
+  db.findById(req.params.id)
+    .then(post => {
+      res.status(200).json(post);
+    })
+    .catch(err => {
+      res.status(407).json({ message: "That user doesn't exist" });
+    });
+});
+
 // // --------------------------------- DELETE -----------------------------------//
 router.delete("/:id", (req, res) => {
   db.remove(req.params.id)
@@ -43,20 +55,28 @@ router.delete("/:id", (req, res) => {
 });
 
 // --------------------------------- PUT -----------------------------------//
-router.put("/:id", async (req, res) => {
-  try {
-    const post = await post.update(req.params.id, req.body);
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "I cant find post! Oh nooo!" });
-    }
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: "Error Error! Cant update post!"
+router.put("/:id", (req, res) => {
+  const { title, contents } = req.body;
+
+  db.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        if (title && contents) {
+          db.update(req.params.id, req.body).then(post => {
+            res.status(200).json(req.body);
+          });
+        } else {
+          res.status(400).json({
+            message: "Please provide a post"
+          });
+        }
+      } else {
+        res.status(404).json({ message: "That post doesn't exist" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Failed to update post" });
     });
-  }
 });
 
 module.exports = router;
